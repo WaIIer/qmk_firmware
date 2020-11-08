96f0419fa7337e970b526e15477eb14f244f3874
Brandon Waller <brandonw@live.com>
2020-11-08 14:28:36 -0500

    fix arrows in default keymap

--- keyboards/WaIIer/keymaps/default/keymap.c
+++ keyboards/WaIIer/keymaps/default/keymap.c
@@ -20,14 +20,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_RGB_ORTHO( // default
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,     KC_U,  KC_I,    KC_O,   KC_P,     KC_BSPC,
-        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,     KC_J,  KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-        KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,     KC_M,  KC_COMM, KC_DOT, KC_SLASH, KC_ENTER,
-        MO(3),     KC_LCPO, KC_LALT, KC_LCMD, MO(1), KC__MUTE, KC_SPACE, MO(2), KC_LEFT, KC_UP,  KC_DOWN,  KC_RIGHT
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,     KC_U,  KC_I,    KC_O,    KC_P,     KC_BSPC,
+        KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,     KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,     KC_M,  KC_COMM, KC_DOT,  KC_SLASH, KC_ENTER,
+        MO(3),     KC_LCPO, KC_LALT, KC_LCMD, MO(1), KC__MUTE, KC_SPACE, MO(2), KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT
     ),
     [1] = LAYOUT_RGB_ORTHO( // lower
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,     KC_RPRN,   KC_DEL,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR,     KC_RCBR,   KC_PIPE,
+          KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR,     KC_RCBR,   KC_PIPE,
         KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PIPE, KC_PIPE, KC_HOME,     KC_END,    KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_TRNS, KC__VOLDOWN, KC__VOLUP, KC_MPLY
     ),
